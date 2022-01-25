59c62c9384e378bc0fa7e013d9b5e69276dff6af
lexmin0412 <zhangle_media@hotmail.com>
2022-01-25 17:53:18 +0800

    chore: 迁移typescript

--- src/lunchtime.ts
+++ src/lunchtime.ts
@@ -6,8 +6,11 @@ export default function getNextLunchtime (hours, minutes) {
 	lunchtime.setSeconds(0);
 	lunchtime.setMilliseconds(0);
 
+	console.log('lunchtime', lunchtime)
+
 	// if we've already had lunch today, start planning
 	// tomorrow's lunch
+	// @ts-ignore
 	if (lunchtime < Date.now()) lunchtime.setDate(lunchtime.getDate() + 1);
 
 	return lunchtime;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import ms from 'ms';
 import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
 
-export default function howLongUntilLunch(hours, minutes) {
+export default function howLongUntil(hours, minutes) {
 	// lunch is at 12.30
 	if (hours === undefined) hours = 12;
 	if (minutes === undefined) minutes = 30;
